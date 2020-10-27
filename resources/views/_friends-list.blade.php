e75461f742dcaeb9d823210364363634bdb6bba0
Schampen <hampus.mannelev.ga.ntig.se>
2020-10-27 10:40:42 +0100

    layout

---
+++ resources/views/_friends-list.blade.php
@@ -0,0 +1,12 @@
+<h3 class="font-bold text-xl mb-4">Friends List</h3>
+
+<ul>
+    @foreach (range(1, 8) as $index)
+        <li class="mb-4">
+            <div class="flex items-center text-sm">
+                <img src="/images/korby.jpg" alt="Korby" class="rounded-full mr-3" style="width:40px;height:40px;">
+                John Doe
+            </div>
+        </li>
+    @endforeach
+</ul>
---
+++ resources/views/_tweet.blade.php
@@ -0,0 +1,13 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-2 flex-shrink-0">
+        <img src="/images/korby.jpg" alt="Korby" class="rounded-full mr-3" style="width:50px;height:50px;">                        
+    </div>
+
+    <div>
+        <h5 class="font-bold mb-4">John Doe</h5>
+
+        <p class="text-sm">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe magni aperiam alias provident aliquam illum quam blanditiis, dignissimos quos praesentium iste ea, quia possimus, laborum harum animi rerum quaerat inventore.
+        </p>
+    </div>
+</div>
