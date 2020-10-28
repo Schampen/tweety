bfdc6042f7c7f8f7d5963140c98731d95f192aef
Schampen <hampus.mannelev.ga.ntig.se>
2020-10-28 09:16:57 +0100

    follow system

--- resources/views/_friends-list.blade.php
+++ resources/views/_friends-list.blade.php
@@ -1,11 +1,11 @@
-<h3 class="font-bold text-xl mb-4">Friends List</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 
 <ul>
-    @foreach (range(1, 8) as $index)
+    @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
             <div class="flex items-center text-sm">
                 <img src="/images/korby.jpg" alt="Korby" class="rounded-full mr-3" style="width:40px;height:40px;">
-                John Doe
+                {{ $user->name }}
             </div>
         </li>
     @endforeach
